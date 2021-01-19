68dc32a695843233a3d329b6ad13b65ce2e35b89
Alex Dominguez <alexdominguez@Alexs-MacBook-Pro.local>
2021-01-18 19:38:07 -0800

    Third Commit.Added second variable that lets users how they want their tea, cold or hot, in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 //Types of teas
 //This variable gives the choices of teas offered
 const teaTypes = ["green","chai", "chamonile", "earlGray"]
-
 //console.log(teaTypes)
+
+//Tea temperatures
+//This variable gives the choices of how the customer wants their tea to be cold or hot
+
+const teaTemperatures = ["cold-iced","cold-blended","cold-no ice","hot"]
+//console.log(teaTemperatures)
