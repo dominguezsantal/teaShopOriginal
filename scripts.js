a355b1f98b454d1fef078121c4c0b9e529180241
dominguezsantal <alexdominguezwebdev@gmail.com>
2021-01-31 06:13:01 -0800

    First object smallColdGreen, according to their properties

--- scripts.js
+++ scripts.js
@@ -1,27 +1,30 @@
 //Tea Constructor and Prototype
-//This function takes the following properties:type of tea, temperature and the size of the object(drink).
+//Created constructor Tea. Tea function takes the following properties:type of tea, temperature and the size of the object(drink).
 
 function Tea(type,temp,size){
   this.teaType = type;
   this.teaTemp = temp;
   this.teaSize = size;
 }
+//Green Tea Prototypes
 
+var smallColdGreen = new Tea("green", "cold", "small");
+console.log(smallColdGreen)
 
 
 //Types of teas
 //This variable gives the choices of teas offered
-//const teaTypes = ["green","chai", "chamonile", "earlGray"]
+///const teaTypes = ["green","chai", "chamonile", "earlGray"]
 //console.log(teaTypes)
 
 
 //Tea temperatures
 //This variable gives the choices of how the customer wants their tea to be cold or hot
-//const teaTemperatures = ["cold-iced","cold-blended","cold-no ice","hot"]
+///const teaTemperatures = ["cold-iced","cold-blended","cold-no ice","hot"]
 //console.log(teaTemperatures)
 
 
 //Tea Sizes
 //This variable gives the choices of what size the customer wants their tea to be
-//const teaSizes = ["Small(8 oz)", "Regular(12 oz)", "Medium(16 oz)", "Large(20 oz)"]
+///const teaSizes = ["Small(8 oz)", "Regular(12 oz)", "Medium(16 oz)", "Large(20 oz)"]
 //console.log(teaSizes)
