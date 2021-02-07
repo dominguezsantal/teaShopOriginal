1cb4529b4475bb5e969a2455edb9ee060c62255b
dominguezsantal <alexdominguezwebdev@gmail.com>
2021-02-07 05:21:06 -0800

    Test 1: Added radiobutton for applying the cost of tea drink based on size selection.Added business logic on scripts.js in which my else if statement takes the size and dispaly the cost of the size.Button test will display the cost of size selected

--- scripts.js
+++ scripts.js
@@ -1,52 +1,40 @@
-//Tea Constructor and Prototype
-//Created constructor Tea. Tea function takes the following properties:type of tea, temperature and the size of the object(drink).
+//COSTS
 
-function Tea(type,temp,size){
-  this.teaType = type;
-  this.teaTemp = temp;
-  this.teaSize = size;
+// Applying the cost of tea drink based on size selection
+
+var sizeCost = [];
+     var size = ("small", "medium", "large", "xlarge")
+      if (size = "small") {
+      sizeCost = 1.70;
+    };
+      if (size = "medium") {
+      sizeCost = 2.25;
+    };
+      if (size = "large") {
+      sizeCost = 2.75;
+    };
+      if (size = "xlarge") {
+      sizeCost = 3.00;
+    };
+
+
+	//console.log('sizeCost');
+
+  var totalCost = (sizeCost);
+
+
+
+
+
+  //Button Javascript
+  //Testing button will display total cost of tea size selected
+$( document ).ready(function() {
+    $('.totalCost').on('click',function(){
+    /** button function with click listener **/
+      buttonFunc();
+    });
+ });
+
+function buttonFunc(){
+alert('Your cost is'+ totalCost);
 }
-//Green Tea Prototypes
-
-//Green Tea Prototypes
-//New objects according to their properties.
-//var smallColdGreen = new Tea("green", "cold", "small");
-//var smallHotGreen = new Tea("green", "hot", "small");
-//var medColdGreen = new Tea("green", "cold", "medium");
-//var medHotGreen = new Tea("green", "hot","medium");
-//var largeColdGreen = new Tea("green", "cold","large");
-var largeHotGreen = new Tea("green", "hot");
-
-
-//console.log(smallColdGreen)
-//console.log(smallHotGreen)
-//console.log(medColdGreen)
-//console.log(medHotGreen)
-//console.log(largeColdGreen)
-console.log(largeHotGreen)
-
-
-
-
-
-
-
-
-
-
-//Types of teas
-//This variable gives the choices of teas offered
-///const teaTypes = ["green","chai", "chamonile", "earlGray"]
-//console.log(teaTypes)
-
-
-//Tea temperatures
-//This variable gives the choices of how the customer wants their tea to be cold or hot
-///const teaTemperatures = ["cold-iced","cold-blended","cold-no ice","hot"]
-//console.log(teaTemperatures)
-
-
-//Tea Sizes
-//This variable gives the choices of what size the customer wants their tea to be
-///const teaSizes = ["Small(8 oz)", "Regular(12 oz)", "Medium(16 oz)", "Large(20 oz)"]
-//console.log(teaSizes)
